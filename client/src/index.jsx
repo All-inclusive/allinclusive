a23c1cce45c710f854ffd8cf20980c7fd98e6f41
Mohamed-aymen-kaabi <68249571+Aymen-kaabi@users.noreply.github.com>
2020-10-27 11:15:06 +0100

    verions compatibility

---
+++ client/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Navbar,Nav,NavDropdown} from 'react-bootstrap';
+
+class App extends React.Component {
+    render(){
+        return(
+            
+        )
+    }
+}
+ReactDOM.render(<App />, document.getElementById('app'));
