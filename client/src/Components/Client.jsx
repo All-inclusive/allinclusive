8ba52da7f7f777654e69dd8a700c5138a8e95014
Mohamed-aymen-kaabi <68249571+Aymen-kaabi@users.noreply.github.com>
2020-10-27 14:56:12 +0100

    .

--- client/src/Components/Client.jsx
+++ client/src/Components/Client.jsx
@@ -1,36 +1,19 @@
 import React, { Component } from "react";
 
-<<<<<<< HEAD
 export default class Client extends Component {
-  
-    render() {
-        return (
-            <div>
-                <ProSidebar>
-                  <Menu iconShape="square">
-                    <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
-                    <SubMenu title="Components" icon={<FaHeart />}>
-                      <MenuItem>Component 1</MenuItem>
-                      <MenuItem>Component 2</MenuItem>
-                    </SubMenu>
-                  </Menu>
-                </ProSidebar>;
-            </div>
-=======
->>>>>>> 148ebc5a8220e0d5c685830f7fd1287673e3a1c4
 
- class Client extends Component  {
-     constructor(props){
-         super(props)
-     }
-
-     render() {
-         return (
+  render() {
+    return (
       <div>
-          
+        <ProSidebar>
+          <Menu iconShape="square">
+            <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
+            <SubMenu title="Components" icon={<FaHeart />}>
+              <MenuItem>Component 1</MenuItem>
+              <MenuItem>Component 2</MenuItem>
+            </SubMenu>
+          </Menu>
+        </ProSidebar>;
       </div>
-    );
-  }
+  )}
 }
-
-export default Client;
