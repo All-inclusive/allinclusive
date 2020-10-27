225618bb33db33618e89da28b94592de912341ff
amira <amirabourogaa0@gmail.com>
2020-10-27 06:27:10 -0700

    changes components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Client from './Components/Client.jsx'
+
 import './style.css'
 class App extends React.Component {
     render(){
         return(
            <div>
-               <Client />
+               
            </div> 
         )
     }
