3bc1a9b733b49eca8d4d987654a419f09c184e7e
amira <amirabourogaa0@gmail.com>
2020-10-27 08:24:23 -0700

    side box ready

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from './Components/Navbar.jsx';
-import AboutUs from './Components/AboutUs.jsx';
-import BecomeASeller from './Components/BecomeASeller.jsx';
-import Company from './Components/Company.jsx';
-import JobOpp from './Components/JobOpp.jsx';
-import SignIn from './Components/SignIn.jsx';
-import SignUp from './Components/SignUp.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "./Components/Navbar.jsx";
+import AboutUs from "./Components/AboutUs.jsx";
+import BecomeASeller from "./Components/BecomeASeller.jsx";
+import Company from "./Components/Company.jsx";
+import JobOpp from "./Components/JobOpp.jsx";
+import SignIn from "./Components/SignIn.jsx";
+import SignUp from "./Components/SignUp.jsx";
+import SideNav from "./Components/SideNav.jsx";
 
-
-import './style.css'
+import "./style.css";
+import Client from "./Components/Client.jsx";
+import { Col, Container, Row } from "react-bootstrap";
 class App extends React.Component {
-    render(){
-        return(
-            <div>
-                <Navbar/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div>
+          <Navbar />
+          <Container fluid>
+            <Row>
+              <Col>
+                <SideNav />
+              </Col>
+              <Col xs={6} style={{overflowY: "scroll"}}>2 of 3</Col>
+              <Col>3 of 3</Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
 }
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
