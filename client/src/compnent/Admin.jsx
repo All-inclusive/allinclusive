a67f9346bcc9f6b56d5422e7594b6c6895da5fe3
oussamasoui <souioussama86@gmail.com>
2020-10-27 17:02:38 +0100

    no changes

--- client/src/compnent/Admin.jsx
+++ client/src/compnent/Admin.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Navbar,Nav,NavDropdown} from 'react-bootstrap';
-import Admin from './compnent/Admin.jsx'
 
-class App extends React.Component {
+class Admin extends React.Component {
     render(){
         return(
             <div>
-            
+                
             </div>
+            
         )
     }
 }
-ReactDOM.render(<App />, document.getElementById('app'));
+export default Admin;
