1c415ebbc97f0378944fc1d9e397db40a2e801d4
unknown <heni.oussama23@gmail.com>
2020-10-28 13:20:22 +0100

    completed validation for the user sign up, installed bcrypt and hashed the password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const User = require('../../database/Schema/Users.js');
+const bcrypt = require('bcryptjs')
+
 
 
 //Validation for user
@@ -8,24 +10,28 @@ const schema = Joi.object({
     userName :Joi.string().min(6).required(),
     email: Joi.string().min(6).required().email(),
     password : Joi.string().min(6).required(),
-    phoneNumber : Joi.number().min(6).required(),
+    phoneNumber : Joi.number().required(),
     specialNeed : Joi.boolean().required(),
     imgUrl : Joi.string().min(6).required()
 })
 
 router.post('/add',async(req,res,next)=>{
-
+     //check if user already exist
     const emailExist = await User.findOne({email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exists')
-try{
-    //check if user already exist
+    //hash password
     
 
+
+
+try{
+    const salt = await bcrypt.genSalt(10)  
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
       const newUser = new User ({
        userName: req.body.userName,
        email:req.body.email,
-       password:req.body.password,
+       password:hashedPassword,
        phoneNumber:req.body.phoneNumber,
        specialNeed :req.body.specialNeed,
        imgUrl :req.body.imgUrl 
