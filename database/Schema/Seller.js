604c4d2d116b0169659a34fc10c6d89296ee0a63
unknown <heni.oussama23@gmail.com>
2020-10-28 14:07:21 +0100

    finished vaerification hasing password on sign up, finished validation on login

--- database/Schema/Seller.js
+++ database/Schema/Seller.js
@@ -5,8 +5,8 @@ const sellerSchema = new mongoose.Schema(
         email : {type:String , required:true},
         password : {type:String , required:true},
         phoneNumber : {type:String , required:true},
-        description :{type:String , required:true},
         imgUrl :{type:String , required:true},
+        description :{type:String , required:true},
     },
     {
         timestamps : true,
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const User = require('../../database/Schema/Users.js');
 const bcrypt = require('bcryptjs')
-
-
-
-//Validation for user
 const Joi = require('joi');
+
+
+//Validation for user registration
+
 const schema = Joi.object({
     userName :Joi.string().min(6).required(),
     email: Joi.string().min(6).required().email(),
@@ -19,12 +19,9 @@ router.post('/add',async(req,res,next)=>{
      //check if user already exist
     const emailExist = await User.findOne({email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exists')
-    //hash password
-    
-
-
 
 try{
+     //hash password
     const salt = await bcrypt.genSalt(10)  
     const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
@@ -40,6 +37,32 @@ try{
         const {error} = await schema.validateAsync(req.body)
         const savedUser = await newUser.save()
         res.send(savedUser)
+    }catch(error){
+        if(error.isJoi === true) res.status(400).send(error.details[0].message)
+        next(error)
+    }
+  });
+
+  //Validation for user login
+
+  
+const loginschema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password : Joi.string().min(6).required(),
+})
+
+router.post('/login',async(req,res,next)=>{
+     //check if user already exist
+    const user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(400).send('Email or password is wrong')
+
+     //check password 
+     const validPass = await bcrypt.compare(req.body.password, user.password)
+     if(!validPass) return res.status(400).send('password not valid')
+
+try{
+        const {error} = await loginschema.validateAsync(req.body)
+        res.send("Logged in")
     }catch(error){
         if(error.isJoi === true) res.status(400).send(error.details[0].message)
         next(error)
