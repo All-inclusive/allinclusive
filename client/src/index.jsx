149127eb626b80a22e040a40adcb7e8454024b6e
amira <amirabourogaa0@gmail.com>
2020-10-29 03:14:18 -0700

    changes for the slide show

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -25,7 +25,7 @@ class App extends React.Component {
                 {/* <Slide />  */}
                 {/* <Events /> */}
 
-                <BecomeASeller/>
+                <SignUpMyAccount/>
             </div>
         )
     }
