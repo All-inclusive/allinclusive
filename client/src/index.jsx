26752d6f14dbb5bde8f94dd7c9e1b5687ccb9f9e
unknown <heni.oussama23@gmail.com>
2020-10-30 09:26:27 +0100

    changed somethings on index client side

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,75 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from './Components/Navbar.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "./Components/Navbar.jsx";
 // import AboutUs from './Components/AboutUs.jsx';
-import BecomeASeller from './Components/BecomeASeller.jsx';
+import BecomeASeller from "./Components/BecomeASeller.jsx";
 // import SignIn from './Components/SignIn.jsx';
-import SignUpMyAccount from './Components/SignUpMyAccount.jsx';
-import Slide from './Components/SlideShow.jsx';
-import Filter from './Components/searchBar.jsx';
-import JobOpp from './Components/JobOpp.jsx';
+import SignUpMyAccount from "./Components/SignUpMyAccount.jsx";
+import Slide from "./Components/SlideShow.jsx";
+import Filter from "./Components/searchBar.jsx";
+import JobOpp from "./Components/JobOpp.jsx";
 // import Client from './Components/Client.jsx';
 // import SideNav from './Components/SideNav.jsx';
 // import Events from './Components/EventsList.jsx';
 
 // import Company from './Components/Company.jsx';
 
-
-
-
-
-
-
 class App extends React.Component {
-    constructor(props){
-        super(props);
-        this.state={
-            view: ''
-        }
-        this.changeVeiw= this.changeView.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: "",
+    };
+    this.changeVeiw = this.changeView.bind(this);
+  }
+
+  changeView(e) {
+    this.setState({ view: e });
+  }
+
+  render() {
+    if (this.state.view === "") {
+      return (
+        <div>
+          <Navbar changeView={(x) => this.changeView(x)} />
+          <Slide />
+          <center>
+            <Filter />
+          </center>
+        </div>
+      );
+    } else if (this.state.view === "My Account") {
+      return (
+        <div>
+          <Navbar changeView={(x) => this.changeView(x)} />
+          <SignUpMyAccount />
+        </div>
+      );
+    } else if (this.state.view === "Become a Seller") {
+      return (
+        <div>
+          <Navbar changeView={(x) => this.changeView(x)} />
+          <BecomeASeller />
+        </div>
+      );
+    } else if (this.state.view === "Job opportunities") {
+      return (
+        <div>
+          <Navbar changeView={(x) => this.changeView(x)} />
+          <JobOpp />
+        </div>
+      );
     }
-
-    changeView(e){
-        this.setState({view: e})       
-    }
-
-    render(){
-        if(this.state.view === ''){
-            return(
-
-                <div>
-                    <Navbar changeView={(x)=>this.changeView(x)}/>
-                    <Slide/> 
-                    <center>
-                    <Filter/>
-                    </center>
-                </div>
-            )
-        }else if (this.state.view === 'My Account'){
-            return(
-                <div>
-                    <Navbar changeView={(x)=>this.changeView(x)}/>
-                    <SignUpMyAccount/>
-                </div>
-            )
-        }else if(this.state.view === 'Become a Seller'){
-            return (
-                <div>
-                    <Navbar changeView={(x)=>this.changeView(x)}/>
-                    <BecomeASeller/>
-                </div>
-            )
-        }else if(this.state.view === 'Job opportunities'){
-            return (
-                <div>
-                    <Navbar changeView={(x)=>this.changeView(x)}/>
-                    <JobOpp/>
-                </div>
-            )
-        }
-        
-        
-    }
-
+  }
 }
 ReactDOM.render(<App />, document.getElementById("app"));
